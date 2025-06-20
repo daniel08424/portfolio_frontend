619b3bf484e66af68c82963557c35bb0526ceea8
daniel08424 <107424572+daniel08424@users.noreply.github.com>
2025-06-20 19:16:19 +0530

    Update AboutMe.jsx

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -6,13 +6,13 @@ export default function AboutMe() {
         <div className="hero--section--content">
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description">
-          Hi, I am a Full Stack Web Developer. An Open Source enthusiast and persuing Electronics and Communication Degree in Sri Eshwar College of Engineering, Coimbatore.
+          Hi, I’m Daniel ImmanSingh, a Full Stack Web Developer and open-source enthusiast. I recently completed my B.E. in Electronics and Communication Engineering at Sri Eshwar College of Engineering, Coimbatore.
           </p>
           <p className="hero--section-description">
-          Although I'm a fresher to the field, I possess a strong foundation in programming and a keen interest in building innovative solutions. While I may not have extensive professional experience yet, I've honed my skills through personal projects . I'm enthusiastic about joining dynamic teams and contributing to meaningful projects. 
+          I have hands-on experience building full-stack applications using technologies like React, Node.js, Express, and MongoDB. Through internships and personal projects, I’ve developed secure, scalable, and high-performance web applications. My recent experience includes working at UST as a Software Developer Trainee, where I contributed to backend development using Java, Spring Boot, and SQL, improving API performance and code reliability through Test-Driven Development. 
           </p>
           <p className="hero--section-description">
-          I love working on new and exciting technologies.  With my passion and keen interest in Web Development I am always looking for new opportunities to learn and grow and provide positive impact in the tech industry.
+          I'm passionate about solving real-world problems with technology, eager to work with dynamic teams, and continuously exploring opportunities to learn, grow, and make a positive impact in the tech industry.
           </p>
           
         </div>
